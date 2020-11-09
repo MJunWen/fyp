a8b0980d4d7a761f94c09c80be2041cab0aa20df
MJunWen <mokjunwen@gmail.com>
2020-11-10 00:21:30 +0800

    second commit

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,18 @@
 var express = require('express');
 var router = express.Router();
 const crypto = require('crypto');
+const password = 'androidencryption';
+const salt = 'F9DB886899A6B';
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
    
-  const secret = 'abcdefg';
-  const hash = crypto.createHmac('sha256', secret)
-                    .update('I love cupcakes')
-                    .digest('hex');
-  console.log(hash);
+  const scrypt = crypto.scryptSync(password, salt, 16);
+  scrypt1hash = scrypt.toString('hex');
 
-  res.render('index', { title: 'Express', hash });
+  res.render('index', { title: 'Express', password, salt, scrypt1hash });
 });
 
 module.exports = router;
