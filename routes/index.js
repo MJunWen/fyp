51f5c23b3f2145d94fc61c7675e58e03efe3f080
MJunWen <mokjunwen@gmail.com>
2021-01-02 00:45:03 +0800

    finish FDE schema

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,15 @@
 var express = require('express');
 var router = express.Router();
 const crypto = require('crypto');
+
 const password = 'androidencryption';
 const salt = crypto.randomBytes(16).toString("hex")
 const frontikpad = "0";
 const backikpad = "0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
 //backikpad is 223 bytes of 0
 //placeholder
+const dek = crypto.randomBytes(64).toString("hex")
+const nonce1 = crypto.randomBytes(16).toString("hex")
 
 //scrypt setup
 const scrypt = crypto.scryptSync(password, salt, 32);
@@ -44,7 +47,12 @@ const rsaencrypted = encryptedData.toString("base64");
 
 //IK3 setup
 const scrypt2 = crypto.scryptSync(rsaencrypted, salt, 32);
-scrypt2hash = scrypt.toString('hex');
+scrypt2hash = scrypt2.toString('hex');
+
+const kek = scrypt2hash.substring(0,32);
+const iv = scrypt2hash.substring(32,65);
+
+
 
 //RSA decrypt setup
 const decryptedData = crypto.privateDecrypt(
@@ -56,6 +64,20 @@ const decryptedData = crypto.privateDecrypt(
 
 const rsadecrypted = decryptedData.toString();
 
+//Per File key Setup 1
+var key = Buffer.from(nonce1,'hex');
+var src = Buffer.from(dek,'hex');
+cipher = crypto.createCipheriv("aes-128-ecb", key, null)
+cipher.setAutoPadding(false)
+result = cipher.update(src).toString('hex');
+result += cipher.final().toString('hex');
+
+
+// const result = "A";
+
+
+
+
 
 
 
@@ -65,7 +87,7 @@ router.get('/', function(req, res, next) {
    
 
   //render lets you import variables into the html file which is index.ejs
-  res.render('index', { title: 'Express', password, salt, frontikpad, backikpad, privateKey, publicKey, scrypt1hash,  rsaencrypted, rsadecrypted,ik1pad,scrypt2hash});
+  res.render('index', { title: 'Express', password, salt,dek, frontikpad, backikpad, privateKey, publicKey, scrypt1hash,  rsaencrypted, rsadecrypted,ik1pad,scrypt2hash,kek,iv,nonce1,result});
 });
 
 module.exports = router;
