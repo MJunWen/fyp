5ebef35f5e5487b7e4a86f33367f10f2feb66fb4
MJunWen <mokjunwen@gmail.com>
2021-01-02 00:45:03 +0800

    finish FDE schema

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,14 @@
 var express = require('express');
 var router = express.Router();
 const crypto = require('crypto');
+
 const password = 'androidencryption';
 const salt = crypto.randomBytes(16).toString("hex")
 const frontikpad = "0";
 const backikpad = "0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
 //backikpad is 223 bytes of 0
 //placeholder
+const dek = crypto.randomBytes(64).toString("hex")
 
 //scrypt setup
 const scrypt = crypto.scryptSync(password, salt, 32);
@@ -44,7 +46,10 @@ const rsaencrypted = encryptedData.toString("base64");
 
 //IK3 setup
 const scrypt2 = crypto.scryptSync(rsaencrypted, salt, 32);
-scrypt2hash = scrypt.toString('hex');
+scrypt2hash = scrypt2.toString('hex');
+
+const kek = scrypt2hash.substring(0,32);
+const iv = scrypt2hash.substring(32,65);
 
 //RSA decrypt setup
 const decryptedData = crypto.privateDecrypt(
@@ -60,12 +65,14 @@ const rsadecrypted = decryptedData.toString();
 
 
 
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
    
 
   //render lets you import variables into the html file which is index.ejs
-  res.render('index', { title: 'Express', password, salt, frontikpad, backikpad, privateKey, publicKey, scrypt1hash,  rsaencrypted, rsadecrypted,ik1pad,scrypt2hash});
+  res.render('index', { title: 'Express', password, salt,dek, frontikpad, backikpad, privateKey, publicKey, scrypt1hash,  rsaencrypted, rsadecrypted,ik1pad,scrypt2hash,kek,iv});
 });
 
 module.exports = router;
