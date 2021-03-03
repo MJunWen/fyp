85172640808f02c531a682d513637a84badadf91
MJunWen <mokjunwen@gmail.com>
2021-03-04 00:02:53 +0800

    file content encryption

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@
 var express = require('express');
 var router = express.Router();
 const crypto = require('crypto');
+const path = require('path');
+const fs = require('fs');
 
 const password = 'androidencryption';
 const salt = crypto.randomBytes(16).toString("hex")
@@ -12,6 +14,8 @@ const backikpad = "0000000000000000000000000000000000000000000000000000000000000
 //placeholder
 const dek = crypto.randomBytes(64).toString("hex")
 const nonce1 = crypto.randomBytes(16).toString("hex")
+const bitkey512 = crypto.randomBytes(64).toString("hex")
+const ivforxts = crypto.randomBytes(16).toString("hex")
 
 //scrypt setup
 const scrypt = crypto.scryptSync(password, salt, 32);
@@ -68,17 +72,49 @@ const iv = scrypt2hash.substring(32,65);
 //Per File key Setup 1
 var key = Buffer.from(nonce1,'hex');
 var src = Buffer.from(dek,'hex');
-cipher = crypto.createCipheriv("aes-128-ecb", key, null)
-cipher.setAutoPadding(false)
-result = cipher.update(src).toString('hex');
-result += cipher.final().toString('hex');
+cipher = crypto.createCipheriv("aes-128-ecb", key, null);
+cipher.setAutoPadding(false);
+pfk1 = cipher.update(src).toString('hex');
+pfk1 += cipher.final().toString('hex');
+
+//aes-256-xts
+var src2 = Buffer.from(pfk1,'hex');
+var key2 = Buffer.from(bitkey512,'hex');
+var sectornumber = Buffer.from(ivforxts,'hex');
+cipherxts = crypto.createCipheriv("aes-256-xts", key2,sectornumber);
+cipherxts.setAutoPadding(false);
+encryptcontent = cipherxts.update(src2).toString('hex');
+encryptcontent += cipherxts.final().toString('hex');
+
+//encrypt file and decrypt file
+function encryptFile(inputFile, outputFile) {
+  const inputData = fs.readFileSync(inputFile);
+  let cipherxts = crypto.createCipheriv("aes-256-xts", key2,sectornumber); //"let" is important or else it causes errors. ????
+  const output = Buffer.concat([cipherxts.update(inputData) , cipherxts.final()]);
+  fs.writeFileSync(outputFile, output);
+}
+
+function decryptFile(inputFile, outputFile) {
+  const inputData = fs.readFileSync(inputFile);
+  const cipher = crypto.createDecipheriv("aes-256-xts", key2, sectornumber);
+  const output = Buffer.concat([cipher.update(inputData) , cipher.final()]);
+  fs.writeFileSync(outputFile, output);
+}
+//tested txt and JPG encryption and decryption works
+const filetoencrypt = "asdasdada.JPG";
+const encryptedfile = "asdasdadaEncrypted.txt"; //show off in txt form to show it's encrypted gibberish. If not just remove the .txt extension
+const decryptedfile = "asdasdadaDecrypted.JPG";
+encryptFile(path.join(__dirname, filetoencrypt), path.join(__dirname, encryptedfile));
+decryptFile(path.join(__dirname, encryptedfile), path.join(__dirname, decryptedfile));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
    
 
   //render lets you import variables into the html file which is index.ejs
-  res.render('index', { title: 'Express', password, salt,dek, frontikpad, backikpad, privateKey, publicKey, scrypt1hash,  rsaencrypted,ik1pad,scrypt2hash,kek,iv,nonce1,result});
+  res.render('index', { title: 'Express', password, salt,dek, frontikpad, backikpad, privateKey, publicKey, scrypt1hash,  rsaencrypted,ik1pad,scrypt2hash,kek,iv,nonce1,pfk1,ivforxts,encryptcontent});
 });
 
 module.exports = router;
+
+//const encOutput = fs.createWriteStream(path.join(__dirname, "filetoencrypt.txt.enc"));
